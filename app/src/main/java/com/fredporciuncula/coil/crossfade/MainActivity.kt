306204a84bb5067b7d1fc91c1c50361f99b37860
Fred Porciúncula <tfcporciuncula@gmail.com>
2023-03-26 13:02:06 +0200

    Add coil with crossfade issue

--- app/src/main/java/com/fredporciuncula/coil/crossfade/MainActivity.kt
+++ app/src/main/java/com/fredporciuncula/coil/crossfade/MainActivity.kt
@@ -3,13 +3,37 @@ package com.fredporciuncula.coil.crossfade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import coil.Coil
+import coil.ImageLoader
+import coil.compose.AsyncImage
+import coil.transition.CrossfadeTransition
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    Coil.setImageLoader(
+      ImageLoader.Builder(this)
+        // When we have the CrossfadeTransition here...
+        .transitionFactory(CrossfadeTransition.Factory())
+        .build()
+    )
     setContent {
-      Text("Hallo")
+      AsyncImage(
+        model = "https://uploads-ssl.webflow.com/63923a65d5aabf0ad18a0ebd/" +
+            "6419ad1b3d99c5758d111c78_mission1a_background_image.jpg",
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        // ...having or not this placeholder will change the scaling of the final image.
+        placeholder = painterResource(R.drawable.placeholder_larger),
+        contentScale = ContentScale.Crop,
+      )
+
+      // If we remove CrossfadeTransition, the placeholder stops affecting the scale, though.
+      // And if we use placeholder (instead of placeholder_larger), scaling isn't affected even with crossfade.
     }
   }
 }
